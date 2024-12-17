dd8e759844723ff1839230fd690fc0e455f004a3
Bianca1500 <biancatavarescordeiro33@gmail.com>
2024-12-17 12:29:20 -0400

    Componente acordeon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,61 +23,166 @@
 			</li>
 		</ul>
 		<hr />
-		<div class="d-grid gap-1">
-			<button
-				class="btn btn-primary"
-				type="button"
-				data-bs-toggle="collapse"
-				data-bs-target="#collapseExample"
-				aria-expanded="false"
-				aria-controls="collapseExample"
-			>
-				Bom bons
-			</button>
-		</div>
-		<div class="collapse" id="collapseExample">
-			<div class="row row-cols-1 row-cols-md-3 g-4">
-				<div class="col">
-          <a class="text-decoration-none" href=""  data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <div class="card mb-3" style="max-width: 540px;">
-						<div class="row g-0">
-							<div class="col-md-4">
-								<img src="/bom bom.webp" class="img-fluid rounded-start" alt="..." />
-							</div>
-							<div class="col-md-8">
-								<div class="card-body">
-									<h5 class="card-title">Card title</h5>
-									<p class="card-text">
-										This is a wider card with supporting text below as a natural lead-in to
-										additional content. This content is a little bit longer.
-									</p>
-									<p class="card-text">
-										<small class="text-body-secondary">Last updated 3 mins ago</small>
-									</p>
-								</div>
+		<div class="accordion" id="accordionExample">
+			<div class="accordion-item">
+				<h2 class="accordion-header">
+					<button
+						class="accordion-button"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#collapseOne"
+						aria-expanded="true"
+						aria-controls="collapseOne"
+					>
+						Bom bons
+					</button>
+				</h2>
+				<div
+					id="collapseOne"
+					class="accordion-collapse collapse show"
+					data-bs-parent="#accordionExample"
+				>
+					<div class="accordion-body">
+						<div class="row row-cols-1 row-cols-md-2 g-4">
+							<div class="col">
+								<a
+									class="text-decoration-none"
+									href=""
+									data-bs-toggle="modal"
+									data-bs-target="#exampleModal"
+								>
+									<div class="card mb-3" style="max-width: 540px;">
+										<div class="row g-0">
+											<div class="col-md-4">
+												<img src="/bom bom.webp" class="img-fluid rounded-start" alt="..." />
+											</div>
+											<div class="col-md-8">
+												<div class="card-body">
+													<h5 class="card-title">Card title</h5>
+													<p class="card-text">
+														This is a wider card with supporting text below as a natural lead-in to
+														additional content. This content is a little bit longer.
+													</p>
+													<p class="card-text">
+														<small class="text-body-secondary">Last updated 3 mins ago</small>
+													</p>
+												</div>
+											</div>
+										</div>
+									</div></a
+								>
+							</div>	<div class="col">
+								<a
+									class="text-decoration-none"
+									href=""
+									data-bs-toggle="modal"
+									data-bs-target="#exampleModal"
+								>
+									<div class="card mb-3" style="max-width: 540px;">
+										<div class="row g-0">
+											<div class="col-md-4">
+												<img src="/bom bom.webp" class="img-fluid rounded-start" alt="..." />
+											</div>
+											<div class="col-md-8">
+												<div class="card-body">
+													<h5 class="card-title">Card title</h5>
+													<p class="card-text">
+														This is a wider card with supporting text below as a natural lead-in to
+														additional content. This content is a little bit longer.
+													</p>
+													<p class="card-text">
+														<small class="text-body-secondary">Last updated 3 mins ago</small>
+													</p>
+												</div>
+											</div>
+										</div>
+									</div></a
+								>
+							</div>	<div class="col">
+								<a
+									class="text-decoration-none"
+									href=""
+									data-bs-toggle="modal"
+									data-bs-target="#exampleModal"
+								>
+									<div class="card mb-3" style="max-width: 540px;">
+										<div class="row g-0">
+											<div class="col-md-4">
+												<img src="/bom bom.webp" class="img-fluid rounded-start" alt="..." />
+											</div>
+											<div class="col-md-8">
+												<div class="card-body">
+													<h5 class="card-title">Card title</h5>
+													<p class="card-text">
+														This is a wider card with supporting text below as a natural lead-in to
+														additional content. This content is a little bit longer.
+													</p>
+													<p class="card-text">
+														<small class="text-body-secondary">Last updated 3 mins ago</small>
+													</p>
+												</div>
+											</div>
+										</div>
+									</div></a
+								>
 							</div>
 						</div>
-					</div></a>
+					</div>
 				</div>
-				<div class="col">
-					<div class="card mb-3" style="max-width: 540px;">
-						<div class="row g-0">
-							<div class="col-md-4">
-								<img src="/bom bom.webp" class="img-fluid rounded-start" alt="..." />
-							</div>
-							<div class="col-md-8">
-								<div class="card-body">
-									<h5 class="card-title">Card title</h5>
-									<p class="card-text">
-										This is a wider card with supporting text below as a natural lead-in to
-										additional content. This content is a little bit longer.
-									</p>
-									<p class="card-text">
-										<small class="text-body-secondary">Last updated 3 mins ago</small>
-									</p>
-								</div>
-							</div>
-						</div>
+			</div>
+			<div class="accordion-item">
+				<h2 class="accordion-header">
+					<button
+						class="accordion-button collapsed"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#collapseTwo"
+						aria-expanded="false"
+						aria-controls="collapseTwo"
+					>
+						Accordion Item #2
+					</button>
+				</h2>
+				<div
+					id="collapseTwo"
+					class="accordion-collapse collapse"
+					data-bs-parent="#accordionExample"
+				>
+					<div class="accordion-body">
+						<strong>This is the second item's accordion body.</strong> It is hidden by default,
+						until the collapse plugin adds the appropriate classes that we use to style each
+						element. These classes control the overall appearance, as well as the showing and hiding
+						via CSS transitions. You can modify any of this with custom CSS or overriding our
+						default variables. It's also worth noting that just about any HTML can go within the
+						<code>.accordion-body</code>, though the transition does limit overflow.
+					</div>
+				</div>
+			</div>
+			<div class="accordion-item">
+				<h2 class="accordion-header">
+					<button
+						class="accordion-button collapsed"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#collapseThree"
+						aria-expanded="false"
+						aria-controls="collapseThree"
+					>
+						Accordion Item #3
+					</button>
+				</h2>
+				<div
+					id="collapseThree"
+					class="accordion-collapse collapse"
+					data-bs-parent="#accordionExample"
+				>
+					<div class="accordion-body">
+						<strong>This is the third item's accordion body.</strong> It is hidden by default, until
+						the collapse plugin adds the appropriate classes that we use to style each element.
+						These classes control the overall appearance, as well as the showing and hiding via CSS
+						transitions. You can modify any of this with custom CSS or overriding our default
+						variables. It's also worth noting that just about any HTML can go within the
+						<code>.accordion-body</code>, though the transition does limit overflow.
 					</div>
 				</div>
 			</div>
@@ -85,25 +190,28 @@
 	</main>
 </div>
 
-
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <div class="row">
-          <div class="col"><img src="/bom bom.webp" alt=""></div>
-          <div class="col">Bom bom recheado sabor uva</div>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
+<div
+	class="modal fade"
+	id="exampleModal"
+	tabindex="-1"
+	aria-labelledby="exampleModalLabel"
+	aria-hidden="true"
+>
+	<div class="modal-dialog modal-dialog-centered modal-xl">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<div class="modal-body">
+					<div class="mx-auto"><img src="/bom bom.webp" alt="" width="50%" /></div>
+					<div>Bom bom recheado sabor uva</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary">Save changes</button>
+			</div>
+		</div>
+	</div>
 </div>
