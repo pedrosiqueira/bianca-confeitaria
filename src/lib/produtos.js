d2a3a79b97ca5658607d6a9a11da9067b2ae46ec
Bianca1500 <biancatavarescordeiro33@gmail.com>
2025-02-04 08:20:56 -0400

    Preços

--- src/lib/produtos.js
+++ src/lib/produtos.js
@@ -14,14 +14,14 @@ export const catálogo = [
                 título: "Bombom Leite Condensado com Nozes",
                 qtd:0,
                 descrição: "Bombom de chocolate ao leite recheado de leite condensado com nozes - 17g. Mínimo de 25 unidades.",
-                preço: " R$3,06 Un",
+                preço: 3.06,
                 imagem: "/bombom_leitecnozes.jpeg"
             },
             {
                 título: "Bombom Brigadeiro Branco",
                 qtd:0,
                 descrição: "Bombom de chocolate branco recheado de brigadeiro tradicional - 17g. Mínimo de 25 unidades.",
-                preço: "R$3,65 Un",
+                preço: 3.65,
                 imagem: "/bombom_brigadeirob.jpeg"
 
             },
@@ -29,7 +29,7 @@ export const catálogo = [
                 título: "Bombom Brigadeiro",
                 qtd:0,
                 descrição: "Bombom de chocolate ao leite recheado de brigadeiro tradicional - 15g. Mínimo de 25 unidades.",
-                preço: "R$3,65 Un",
+                preço: 3.65,
                 imagem: "/bombom_brigadeiro.jpeg"
 
             }]
@@ -42,22 +42,22 @@ export const catálogo = [
                 título: "Bolo no pote holandesa",
                 qtd:0,
                 descrição: "Biscoito amanteigado, creme holandês e ganache de chocolate. Foto ilustrativa. Manter refrigerado. Pote com 210ml",
-                preço: "R$14,90",
+                preço: 14.90,
                 imagem: "/bolo_holandes.jpeg"
             },
             {
                 título: "Bolo no pote 4 leites brigadeiro",
                 qtd:0,
                 descrição: "Bolo de chocolate, recheio de quatro leites e brigadeiro. Foto ilustrativa. Manter refrigerado. Pote com 210ml.",
-                preço: "R$14,90",
-                desconto: "R$13,40",
+                preço: 14.90,
+                desconto: 13.40,
                 imagem: "/bolo_4leites_brigadeiro.jpeg"
             },
             {
                 título: "Bolo no pote 4 leites branco",
                 qtd:0,
                 descrição: "Bolo branco, creme quatro leites, chantili e ganache branco. Foto ilustrativa. Manter refrigerado. Pote com 210ml.",
-                preço: "R$14,90",
+                preço: 14.90,
                 imagem: "/bolo_4leites_branco.jpeg"
             }]
     },
@@ -69,29 +69,29 @@ export const catálogo = [
                 título: "Torta Ovomaltine",
                 qtd:0,
                 descrição: "Bolo de chocolate, musse trufado branco, flocos de ovomaltine e brigadeiro. Foto ilustrativa. Manter refrigerado.",
-                preço: "R$72,90 Kg",
+                preço: 72.90,
                 imagem: "/torta_ovomaltine.jpeg"
             },
             {
                 título: "Torta Café Trufada",
                 qtd:0,
                 descrição: "Bolo de chocolate, musse de chocolate e café, doce de leite. Foto ilustrativa. Manter refrigerado.",
-                preço: "R$72,90 Kg",
-                desconto: "R$70,00",
+                preço: 72.90,
+                desconto: 70.00,
                 imagem: "/torta_cafe.jpeg"
             },
             {
                 título: "Torta Quatro Leites Branca",
                 qtd:0,
                 descrição: "Bolo branco, creme quatro leites, chantili e ganache branco. Foto ilustrativa. Manter refrigerado.",
-                preço: "R$74,90 Kg",
+                preço: 74.90,
                 imagem: "/torta_branca.jpeg"
             },
             {
                 título: "Torta Meio Amarga",
                 qtd:0,
                 descrição: "Bolo de chocolate, ganache, creme três leites, musse de chocolate meio amargo . Foto ilustrativa. Manter refrigerado",
-                preço: "R$76,90 Kg",
+                preço: 76.90,
                 imagem: "/torta_meioam.jpeg"
             }]
     },
@@ -103,22 +103,22 @@ export const catálogo = [
                 título: "Maracujá 250 ml",
                 qtd:0,
                 descrição: "Geléia de maracujá artesanal, mousse de maracujá suave, mousse de maracujá, brownie, ganache de chocolate, chantilly 250ml",
-                preço: "R$15,90",
+                preço: 15.90,
                 imagem: "/copo_maracuja.jpeg"
             },
             {
                 título: "Morango 250 ml",
                 qtd:0,
                 descrição: "Geleia artesanal de Morango, creme branco, Brownie, brigadeiro belga, 250m",
-                preço: "R$19,90",
-                desconto: "R$15,00",
+                preço: 19.90,
+                desconto: 15.00,
                 imagem: "/copo_morango.jpeg"
             },
             {
                 título: "Floresta negra 250 ml",
                 qtd:0,
                 descrição: "Brownie, creme de 4leites, cereja em calda, brigadeiro, raspas de chocolate e chantilly 250 ml",
-                preço: "R$18,90",
+                preço: 18.90,
                 imagem: "/copo_florestanegra.jpeg"
             }]
     }
